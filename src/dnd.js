7358914b45a98f72dc7de446844fa654c384a1d8
Максим <maxim_91@inbox.ru>
2017-12-06 22:34:36 +0300

    dnd complete

--- src/dnd.js
+++ src/dnd.js
@@ -25,10 +25,10 @@ let homeworkContainer = document.querySelector('#homework-container');
 function createDiv() {
 
     function randomNumber(min = 0, max = 100, flag = false) {
-        return flag ? Math.round((max - min) * Math.random()) + min + 'px' : Math.round((max - min) * Math.random()) + min
+        return flag ? Math.round((max - min) * Math.random()) + min + 'px' : Math.round((max - min) * Math.random()) + min;
     }
     function generateColor() {
-        return '#' + Math.floor(Math.random() * 16777215).toString(16)
+        return '#' + Math.floor(Math.random() * 16777215).toString(16);
     }
     let element = document.createElement('div'),
         styleWidth = randomNumber(50, 150),
@@ -39,8 +39,10 @@ function createDiv() {
     element.style.height = styleHeight + 'px';
     element.style.backgroundColor = generateColor();
     element.style.position = 'absolute';
-    element.style.top = randomNumber(0, homeworkContainer.offsetHeight - styleHeight) + 'px';
-    element.style.left = randomNumber(0, homeworkContainer.offsetWidth - styleWidth) + 'px';
+    element.style.top =
+        randomNumber(0, homeworkContainer.offsetHeight - styleHeight) + 'px';
+    element.style.left =
+        randomNumber(0, homeworkContainer.offsetWidth - styleWidth) + 'px';
 
     return element;
 }
@@ -51,11 +53,84 @@ function createDiv() {
  * @param {Element} target
  */
 function addListeners(target) {
+    function getCoords(elem) {
+        let box = elem.getBoundingClientRect();
+
+        return {
+            top: box.top + pageYOffset,
+            left: box.left + pageXOffset,
+            width: box.width,
+            height: box.height
+        };
+    }
+
+    target.addEventListener('mousedown', e => {
+        let style = window.getComputedStyle(homeworkContainer, null);
+        let shiftX = e.pageX - getCoords(target).left;
+        let shiftY = e.pageY - getCoords(target).top;
+        let borderRight = getCoords(homeworkContainer).width - getCoords(target).width - 2 * parseInt(style.borderWidth);
+        let borderBottom = getCoords(homeworkContainer).height - getCoords(target).height - 2 * parseInt(style.borderWidth);
+
+        moveAt(e);
+
+        function moveAt(e) {
+            let coordX = e.pageX - getCoords(homeworkContainer).left - shiftX;
+            let coordY = e.pageY - getCoords(homeworkContainer).top - shiftY;
+
+            target.style.left = coordX + 'px';
+            target.style.top = coordY + 'px';
+
+            function isLeftBound(coordX) {
+                return coordX < 0;
+            }
+
+            function isTopBound(coordY) {
+                return coordY < 0;
+            }
+
+            function isRightBound(coordX) {
+                return coordX > borderRight;
+            }
+
+            function isBottomBound(coordY) {
+                return coordY > borderBottom;
+            }
+
+            if (isLeftBound(coordX)) {
+                target.style.left = 0 + 'px';
+            }
+
+            if (isRightBound(coordX)) {
+                target.style.left = borderRight + 'px';
+            }
+
+            if (isTopBound(coordY)) {
+                target.style.top = 0 + 'px';
+            }
+
+            if (isBottomBound(coordY)) {
+                target.style.top = borderBottom + 'px';
+            }
+        }
+
+        document.onmousemove = function(e) {
+            moveAt(e);
+        };
+
+        document.onmouseup = function() {
+            document.onmousemove = null;
+            document.onmouseup = null;
+        };
+
+        target.ondragstart = function() {
+            return false;
+        };
+    });
 }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
 
-addDivButton.addEventListener('click', function () {
+addDivButton.addEventListener('click', function() {
     // создать новый div
     let div = createDiv();
 
@@ -67,6 +142,4 @@ addDivButton.addEventListener('click', function () {
     // или использовать HTML5 D&D - https://www.html5rocks.com/ru/tutorials/dnd/basics/
 });
 
-export {
-    createDiv
-};
+export { createDiv };
